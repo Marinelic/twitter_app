7f52b5f3ee9cdb62c03290cf4dff155ebf041cec
Marinelic <75620592+Marinelic@users.noreply.github.com>
2025-02-17 21:42:38 +0100

    Adding Feed and First Post

---
+++ src/Post.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import PublishIcon from '@mui/icons-material/Publish';
+
+function Post({
+    dispayName,
+    username,
+    verified,
+    text,
+    image,
+    avatar
+}) {
+  return (
+    <div className='post'>
+      <div className='post__avatar'>
+         <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+      </div>
+
+      <div className='post__body'> 
+        <div className='post__header'>  
+            <div className='post__headerText'>
+                <h3>Kygo{""} <span><VerifiedUserIcon className='post__badge'/> @KygoMusic</span></h3>
+            </div>
+
+            <div className='post__headerDescription'>
+                <p>Blablaablaabalaaaaablaaaabla</p>
+            </div>
+        </div>
+
+        <div>
+          <img src='https://media.tenor.com/-ynlGrEzc1MAAAAM/kygo.gif' alt='' />  
+
+          <div className='post__footer'> 
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post;
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './TweetBox.css';
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 
 function TweetBox() {
   return (
-    <div className='tweetbox'>
+    <div className='tweetBox'>
       <form>
         <div className='tweetBox__input'>
             <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
 
             <input placeholder="What's happening?" type='text'></input>
         </div>
-            <button>Tweet</button>
+
+         <input className='tweetBox__imageInput' placeholder="Optional: Enter image URL" type='text'></input>
+
+            <Button className='tweetBox__tweetButton'>Tweet</Button>
       </form>
     </div>
   )
